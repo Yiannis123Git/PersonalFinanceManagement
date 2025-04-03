a9ccce5e3d744ab30355861b2b74e3af2b8613c1
Yiannis123Git <140423132+Yiannis123Git@users.noreply.github.com>
2025-04-03 13:40:02 +0300

    Add save utility and data folder initialization

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 
 from PySide6.QtGui import QIcon
@@ -7,13 +8,36 @@ from PySide6.QtWidgets import QApplication
 # Import qrc resources
 from ui import qml_rc  # noqa: F401
 
+# Import utility functions
+from utility import save
+
+
+def parse_args() -> argparse.Namespace:
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--temp-instance",
+        type=bool,
+        help="Creates a temp app instance. Data will not be saved.",
+        default=False,
+    )
+
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
+    # Get command line arguments
+    cl_args = parse_args()
+
     app = QApplication(sys.argv)
     QApplication.setOrganizationName("HLE43-3")
     QApplication.setApplicationName("Personal Finance Management")
 
     # set application icon
     app.setWindowIcon(QIcon(":/ui/assets/images/app-icon.png"))
+
+    # Establish saved data location
+    save.instantiate(cl_args.temp_instance)
 
     engine = QQmlApplicationEngine()
 
